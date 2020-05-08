19a4550185d1bf509e44fbc32ee493e613053e8c
Vishal Gupta <vishal.kumar1145@gmail.com>
2020-05-08 19:40:09 +0530

    push changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 import { AuthServiceService } from './services/Auth/auth-service.service';
 import { FirestoreService } from './services/firestore/firestore.service';
 import { UtilsServiceService } from './services/Utils/utils-service.service';
+import { FirebaseX } from "@ionic-native/firebase-x/ngx";
 
 @Component({
     selector: 'app-root',
@@ -99,7 +100,8 @@ export class AppComponent {
         private socialSharing: SocialSharing,
         public authService: AuthServiceService,
         public fireStore: FirestoreService,
-        public util: UtilsServiceService
+        public util: UtilsServiceService,
+        private firebase: FirebaseX
     ) {
         this.initializeApp();
         this.authService.userId.subscribe(filter => {
@@ -136,6 +138,18 @@ export class AppComponent {
         this.platform.ready().then(() => {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
+            this.initPush();
+        });
+    }
+
+    initPush(){
+        this.firebase.getToken().then(token => {
+            console.log(`The token is ${token}`)
+            alert(token);
+        })
+        this.firebase.onMessageReceived().subscribe(data => {
+            alert(data);
+            console.log(`FCM message: ${data}`)
         });
     }
 
