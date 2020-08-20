e21ea2f9e54c4ddc3ce2420837346fefd0bf427a
Santosh <santosh.boddupalli@kehe.com>
2020-08-20 14:53:49 -0500

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase:  {
-    apiKey: 'AIzaSyDnuVc6ztm7hh5dalmyJXZ4aSteAnmarT4',
-    authDomain: 'gretello-b983f.firebaseapp.com',
-    databaseURL: 'https://gretello-b983f.firebaseio.com',
-    projectId: 'gretello-b983f',
-    storageBucket: 'gretello-b983f.appspot.com',
-    messagingSenderId: '994233102537',
-    appId: '1:994233102537:web:7e17dc82bae4ba65ca9d18',
-    measurementId: 'G-PBWL18MT76'
+    apiKey: 'AIzaSyBC9-NGcvif5Hl-_2mwapfJl7C_v6YcgN0',
+    authDomain: 'gretello-dev.firebaseapp.com',
+    databaseURL: 'https://gretello-dev.firebaseio.com',
+    projectId: 'gretello-dev',
+    storageBucket: 'gretello-dev.appspot.com',
+    messagingSenderId: '811864497792',
+    appId: '1:811864497792:web:2bcd4e7b2365cadf331065',
+    measurementId: 'G-2S6CB9TNVP'
   }
 };
